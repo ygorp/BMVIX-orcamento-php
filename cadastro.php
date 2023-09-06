842ed70e760940be0172ef92a5b2da799125508a
ygorp <ygor-nogueira@hotmail.com>
2023-09-06 16:06:23 -0300

    criando o front end de um sistema de orçamento

---
+++ cadastro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMVIX - Cadastro</title>
+
+    <link rel="stylesheet" href="css/cadastro.css">
+
+</head>
+<body>
+<div class="cadastro">
+        <form action="" method="post">
+            <h2>Faça seu cadastro!</h2>
+            <input type="text" name="nome" id="nome" placeholder="Digite seu nome">
+            <input type="email" name="email" id="email" placeholder="Digite seu email">
+            <input type="password" name="senha" id="senha" placeholder="Digite sua senha">
+
+            <div class="btns-cadastro">
+                <input type="submit" value="Cadastrar" id="btn-cadastro">
+                <input type="submit" value="Cancelar" id="btn-cadastro">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMVIX - Orçamento</title>
+
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+    <div class="login">
+        <form action="" method="post">
+            <h2>Faça Login no sistema!</h2>
+            <input type="email" name="email" id="email" placeholder="Digite seu email">
+            <input type="password" name="senha" id="senha" placeholder="Digite sua senha">
+
+            <div class="links">
+                <a href="#">Esqueceu sua senha?</a> | <a href="#">Cadastre-se</a>
+            </div>
+
+            <input type="submit" value="Entrar" id="btn-login">
+        </form>
+    </div>
+</body>
+</html>

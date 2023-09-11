3be573fe8fc6b7c1ff0170a463f4854f474d0aa2
ygorp <ygor-nogueira@hotmail.com>
2023-09-11 11:01:53 -0300

    criando a seção home

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMVIX - Home</title>
+
+    <link rel="stylesheet" href="css/home.css">
+
+</head>
+<body>
+    <header class="topo menu">
+        <img src="/img/logo.png" alt="Logo empresa BMVIX">
+        <nav class="nav">
+            <ul class="menu">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Orçamento</a></li>
+            </ul>
+        </nav>
+        <div class="pesquisa">
+            <input type="search" name="pesquisa" id="pesquisa" placeholder="Digite CNPJ do cliente">
+        </div>
+    </header>
+</body>
+</html>

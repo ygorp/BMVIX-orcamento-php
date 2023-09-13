b0b311bda61aeedbb78b67077e222e89d5b5827d
ygorp <ygor-nogueira@hotmail.com>
2023-09-13 11:44:46 -0300

    atualizando o visual da página home

--- home.php
+++ home.php
@@ -9,35 +9,74 @@
 
 </head>
 <body>
-    <header class="topo-menu">
-        <img src="/img/logo.png" alt="Logo empresa BMVIX">
-        <nav class="nav">
-            <ul class="menu">
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Orçamento</a></li>
-            </ul>
-        </nav>
-        <div class="pesquisa">
-            <input type="search" name="pesquisa" id="pesquisa" placeholder="Digite CNPJ do cliente">
-            <button class="pesquisa">Pesquisar</button>
-        </div>
+    <header class="topo">
+        <img src="/img/logo.png" alt="Logo empresa bmvix">
+        <ul class="menu">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Orçamentos</a></li>
+        </ul>
     </header>
 
     <main>
-        <form action="" method="post" id="form-orcamento">
-            <input type="text" name="nome" id="nome" placeholder="Digite o nome do cliente">
-            <input type="text" name="cnpj" id="cnpj" placeholder="CNPJ cliente">
-            <label for="relogio">Possui algum relógio:</label>
-            <select name="relogio" id="relogio">
-                <option value="sim">Sim</option>
-                <option value="nao">Não</option>
-            </select>
-            <input type="text" name="tipo-rep" id="tipo-rep" placeholder="Inmetro ou não">
-            <input type="text" name="app" id="app" placeholder="Precisa de aplicativo">
-            <input type="text" name="facial" id="facial" placeholder="Vai utilizar facial">
-            <input type="text" name="funcionario" id="funcionario" placeholder="Quantos funcionários">
-
-            <input type="submit" value="Gerar Orçamento" id="btn-orcamento">
+        <form action="" method="post">
+            <div>
+                <label for="nome">Digite o nome do cliente</label>
+                <input type="text" name="nome" id="nome"> 
+            </div>
+            <div>
+                <label for="cnpj">Digite o CNPJ</label>
+                <input type="text" name="cnpj" id="cnpj"> 
+            </div>
+            <div>
+                <label for="funcionarios">Quantos funcionarios</label>
+                <input type="text" name="funcionarios" id="funcionarios"> 
+            </div>
+            <div class="app-facial">
+                <div class="app">
+                    <label for="aplicativo">Vai utilizar aplicativo</label>
+                    <select name="aplicativo" id="aplicativo">
+                        <option value="sim">Sim</option>
+                        <option value="nao">Não</option>
+                    </select>
+                </div>             
+                <div class="facial">
+                    <label for="facial">Vai utilizar Facial</label>
+                    <select name="facial" id="facial">
+                        <option value="sim">Sim</option>
+                        <option value="nao">Não</option>
+                    </select> 
+                </div>
+                
+            </div>
+            <div class="id-rep">
+                <div class="controlid">
+                    <label for="controlid">Possui control ID</label>
+                    <select name="controlid" id="controlid">
+                        <option value="sim">Sim</option>
+                        <option value="nao">Não</option>
+                    </select>
+                </div>
+                
+                <div class="relogio">
+                    <label for="relogio">Possui relógio de ponto</label>
+                    <select name="relogio" id="relogio">
+                        <option value="sim">Sim</option>
+                        <option value="nao">Não</option>
+                    </select> 
+                </div>
+            </div>
+            
+            <div class="tipo-rep">
+                <label for="tipo">Qual tipo do relógio</label>
+                <select name="tipo" id="tipo">
+                    <option value="inmetro">Inmetro</option>
+                    <option value="naoinmetro">Não Inmetro</option>
+                </select> 
+            </div>
+            
+           <div class="button">
+                <input type="submit" value="Gerar orçamento">
+           </div>
         </form>
     </main>
 </body>
